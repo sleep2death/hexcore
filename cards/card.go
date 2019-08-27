1924cfd494534c85e45ea0279c63e3aa71075546
aspirin <aspirin2d@outlook.com>
2019-08-28 03:56:48 +0800

    refactory of cards

--- cards/card.go
+++ cards/card.go
@@ -3,10 +3,6 @@ package cards
 import (
 	"errors"
 	"math/rand"
-	"strings"
-	"sync"
-
-	"github.com/lithammer/shortuuid"
 )
 
 var (
@@ -26,274 +22,45 @@ var (
 	ErrPileIsNilOrEmpty = errors.New("target pile is nil or empty")
 )
 
-// Target is the target type of the card
-type Target uint
-
-const (
-	// Enemy as the card target
-	Enemy Target = iota + 1
-	// AllEnemies as the card targets
-	AllEnemies
-	// Self as the card targets
-	Self
-	// None target
-	None
-	// SelfAndEnemy as the card targets
-	SelfAndEnemy
-	// All as the card targets
-	All
-)
-
-func (c Target) String() string {
-	return [...]string{"nil", "enemy", "allEnemy", "self", "none", "selfAndEnemy", "all"}[c]
+// Card - interface
+type Card interface {
+	Upgrade() error
+	String() string
+	ID() string
 }
 
-// CType is the type of the card
-type CType uint
+// Pile of the cards
+type Pile []Card
 
-const (
-	// Attack card type
-	// A reusable card (Unless it has Exhaust) that deals direct damage to an enemy and may have a secondary effect.
-	Attack CType = iota
-	// Skill card type
-	// A reusable card (Unless it has Exhaust) that has more unique effects to it. There isn't a clear direction with offensiveness and defensiveness unlike attacks.
-	Skill
-	// Power card type
-	// A permanent upgrade for the entire combat encounter. Some Powers give flat stats like Strength or Dexterity. Others require certain conditions to be met that combat. Each copy of a given power can only be played once per combat.
-	Power
-	// StatusT card type
-	// Unplayable cards added to the deck during combat encounters. They are designed to bloat the deck and prevent the player from drawing beneficial cards, with some of them having additional negative effects. Unlike Curses, Status cards are removed from the deck at the end of combat.
-	StatusT
-	// CurseT card type
-	// Unplayable cards added to the deck during in-game events. Similar to status cards they are designed to bloat the deck and prevent the player from drawing beneficial cards, with some of them having additional negative effects during combat. Unlike Statuses, Curse cards persist in the players' deck until removed by other means.
-	CurseT
-)
-
-func (c CType) String() string {
-	return [...]string{"attack", "skill", "power", "status", "curse"}[c]
+// Shuffle the cards
+func (p *Pile) Shuffle(seed *rand.Rand) {
+	rand.Shuffle(len(*p), func(i, j int) { (*p)[i], (*p)[j] = (*p)[j], (*p)[i] })
 }
 
-// Status hold all changeable attributes of the card
-type Status struct {
-	Cost   int
-	Damage int
-	Block  int
-	Heal   int
-	Level  int
-	Target Target
-}
-
-// Copy the status
-func (n *Status) Copy() (s *Status) {
-	s = &Status{
-		Cost:   n.Cost,
-		Damage: n.Damage,
-		Block:  n.Block,
-		Heal:   n.Heal,
-		Target: n.Target,
-		Level:  n.Level,
-	}
-	return
-}
-
-// Upgrade a numbers with another numbers
-func (n *Status) Upgrade(u *Status) (s *Status) {
-	s = &Status{
-		Cost:   n.Cost + u.Cost,
-		Damage: n.Damage + u.Damage,
-		Block:  n.Block + u.Block,
-		Heal:   n.Heal + u.Heal,
-		Target: n.Target + u.Target, // upgdate to the target number; if the u.Target == 0, then target not change
-		Level:  n.Level + 1,         // update level + 1
-	}
-
-	return
-}
-
-// Card -
-type Card struct {
-	id    string
-	name  string
-	ctype CType
-
-	base    *Status
-	upgrade *Status
-	current *Status
-}
-
-// Copy the card
-func (card *Card) Copy() *Card {
-	return &Card{
-		// generate a new uuid for the card
-		id: shortuuid.New(),
-		// card name
-		name: card.name,
-		// card type
-		ctype: card.ctype,
-		// some cards may change the base status permantly in the battle
-		// like card [Ritual Dagger] -  if this card kills an enemy then permanently increase this card's damage by 3(5)
-		// if card["feed"] upgraded in the battle, then original card in the deck will also be upgraded
-		// battle manager can use "base" status permanently change the card
-		base: card.base,
-		// card upgrade status will never be modified after created, so use a pointer is fine
-		// when upgrade a card, use the base status add the upgrade status, then return the new upgraded status
-		upgrade: card.upgrade,
-		// card current status in battle
-		current: card.base.Copy(),
-	}
-}
-
-func (card *Card) String() string {
-	return card.name
-}
-
-// Name of the card
-func (card *Card) Name() string {
-	return card.name
-}
-
-// ID return the uuid of the card
-func (card *Card) ID() string {
-	return card.id
-}
-
-// CType return the type of the card
-func (card *Card) CType() CType {
-	return card.ctype
-}
-
-// Base return the base status of the card
-func (card *Card) Base() *Status {
-	return card.base
-}
-
-// Upgrade return the upgrade of the card
-func (card *Card) Upgrade() *Status {
-	return card.upgrade
-}
-
-// UpgradeBase - upgrade card base status, also tit will upgrade the current status
-func (card *Card) UpgradeBase() {
-	card.base = card.base.Upgrade(card.upgrade)
-	card.current = card.current.Upgrade(card.upgrade)
-}
-
-// UpgradeCurrent - upgrade card current status
-func (card *Card) UpgradeCurrent() {
-	card.current = card.current.Upgrade(card.upgrade)
-}
-
-// Current return the current status of the card
-func (card *Card) Current() *Status {
-	return card.current
-}
-
-// CreateCard -
-func CreateCard(name string, ctype CType, base *Status, upgrade *Status) *Card {
-	return &Card{
-		// generate a new uuid for the card
-		id: shortuuid.New(),
-		// card name
-		name: name,
-		// card type
-		ctype: ctype,
-		// some cards may change the base status permantly in the battle
-		// like card [Ritual Dagger] -  if this card kills an enemy then permanently increase this card's damage by 3(5)
-		// if card["feed"] upgraded in the battle, then original card in the deck will also be upgraded
-		// battle manager can use "base" status permanently change the card
-		base: base,
-		// card upgrade status will never be modified after created, so use a pointer is fine
-		// when upgrade a card, use the base status add the upgrade status, then return the new upgraded status
-		upgrade: upgrade,
-		// card current status in battle
-		current: base.Copy(),
-	}
-}
-
-// Pile of cards
-type Pile struct {
-	cards []*Card
-	// Lock
-	mux sync.Mutex
-}
-
-// PileName -
-type PileName int
-
-const (
-	// Deck Pile
-	Deck PileName = iota
-	// Draw Pile
-	Draw
-	// Hand Pile
-	Hand
-	// Discard Pile
-	Discard
-	// Exaust Pile
-	Exaust
-)
-
-// String -
-func (p *Pile) String() string {
-	p.mux.Lock()
-	defer p.mux.Unlock()
-
-	s := ""
-	for _, card := range p.cards {
-		s += card.String() + " "
-	}
-
-	return "[" + strings.TrimSpace(s) + "]"
-}
-
-// Num - get the card number of the pile
-func (p *Pile) Num() int {
-	p.mux.Lock()
-	defer p.mux.Unlock()
-	// l := len(p.cards)
-	return len(p.cards)
-}
-
-// Clear the pile
-func (p *Pile) Clear() {
-	p.mux.Lock()
-	defer p.mux.Unlock()
-
-	p.cards = nil
-}
-
-// AddToTop with the given card(s)
-func (p *Pile) AddToTop(c ...*Card) {
-	p.mux.Lock()
-	defer p.mux.Unlock()
-	p.cards = append(p.cards, c...)
-}
-
-// AddToBottom with the given card(s)
-func (p *Pile) AddToBottom(c ...*Card) {
-	p.mux.Lock()
-	defer p.mux.Unlock()
-	p.cards = append(c, p.cards...)
-}
-
-// Draw n card(s) from the source pile
-func (p *Pile) Draw(source *Pile, n int) error {
-	if n <= 0 {
-		return ErrDrawNumber
-	}
-	if n > source.Num() {
+// Draw one card from the source pile
+func (p *Pile) Draw(source *Pile) error {
+	if len(*source) == 0 {
 		return ErrNotEnoughCards
 	}
 
-	p.mux.Lock()
-	idx := len(source.cards) - n
-	p.cards = append(p.cards, source.cards[idx:]...)
-	p.mux.Unlock()
-
-	source.mux.Lock()
-	source.cards = source.cards[:idx]
-	source.mux.Unlock()
+	*p = append(*p, (*source)[len(*source)-1])
+	*source = (*source)[:len(*source)-1]
 	return nil
+}
+
+// FindCard return both the card and card index of the pile
+func (p *Pile) FindCard(id string) (card Card, idx int, err error) {
+	if len(*p) == 0 {
+		return nil, -1, ErrPileIsNilOrEmpty
+	}
+
+	for i, c := range *p {
+		if c.ID() == id {
+			return c, i, nil
+		}
+	}
+
+	return nil, -1, ErrCardNotExist
 }
 
 // Pick one card from source pile and add it to the top of the pile
@@ -303,105 +70,11 @@ func (p *Pile) Pick(source *Pile, id string) error {
 		return err
 	}
 
-	p.AddToTop(card)
-	source.RemoveCard(idx)
-	return nil
-}
+	*p = append(*p, card)
 
-// RemoveCard from the pile
-func (p *Pile) RemoveCard(i int) (*Card, error) {
-	p.mux.Lock()
-	defer p.mux.Unlock()
-
-	if i < 0 || i > len(p.cards)-1 {
-		return nil, ErrDrawIndex
-	}
-	card := p.cards[i]
-	copy(p.cards[i:], p.cards[i+1:])
-	p.cards = p.cards[:len(p.cards)-1]
-	return card, nil
-}
-
-// GetCard from the pile
-func (p *Pile) GetCard(i int) (*Card, error) {
-	p.mux.Lock()
-	defer p.mux.Unlock()
-	if i < 0 || i > len(p.cards)-1 {
-		return nil, ErrDrawIndex
-	}
-	card := p.cards[i]
-	return card, nil
-}
-
-// FindCard return both the card and card index of the pile
-func (p *Pile) FindCard(id string) (card *Card, idx int, err error) {
-	p.mux.Lock()
-	defer p.mux.Unlock()
-	if len(p.cards) == 0 {
-		return nil, -1, ErrPileIsNilOrEmpty
-	}
-
-	for i, c := range p.cards {
-		if c.ID() == id {
-			return c, i, nil
-		}
-	}
-
-	return nil, -1, ErrCardNotExist
-}
-
-// Shuffle the pile
-func (p *Pile) Shuffle(seed *rand.Rand) {
-	p.mux.Lock()
-	defer p.mux.Unlock()
-
-	if len(p.cards) == 0 {
-		return
-	}
-
-	seed.Shuffle(len(p.cards), func(i, j int) { p.cards[i], p.cards[j] = p.cards[j], p.cards[i] })
-}
-
-// CopyCardsFrom -
-func (p *Pile) CopyCardsFrom(source *Pile) error {
-	if source == nil || source.Num() == 0 {
-		return ErrPileIsNilOrEmpty
-	}
-
-	source.mux.Lock()
-	for _, card := range source.cards {
-		p.AddToTop(card.Copy())
-	}
-	source.mux.Unlock()
+	card = (*source)[idx]
+	copy((*source)[idx:], (*source)[idx+1:])
+	*source = (*source)[:len(*source)-1]
 
 	return nil
-}
-
-// CreatePile by given seed and cardset
-func CreatePile(cardSet []string) (p *Pile, err error) {
-	p = &Pile{}
-
-	if cardSet == nil || len(cardSet) == 0 {
-		return p, nil
-	}
-
-	for _, s := range cardSet {
-		if CreateCardFunc[s] == nil {
-			// clear all the items reference by setting the slice to nil
-			// see: https://stackoverflow.com/questions/16971741/how-do-you-clear-a-slice-in-go
-			p.cards = nil
-			return nil, ErrCardNotExist
-		}
-
-		card := CreateCardFunc[s]()
-		p.AddToTop(card)
-	}
-	return
-}
-
-// CreateCardFunc map for generating cards
-var CreateCardFunc = map[string](func() *Card){
-	"Strike": CreateCardStrike,
-	"Bash":   CreateCardBash,
-	"Defend": CreateCardDefend,
 }
--- cards/card_test.go
+++ cards/card_test.go
@@ -1,259 +1,98 @@
 package cards
 
 import (
+	"errors"
+	"fmt"
 	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCard(t *testing.T) {
-	cardA := CreateCardFunc["Strike"]()
-	assert.EqualValues(t, cardA.Name(), "Strike")
-
-	// Upgrade the card
-	cardA.UpgradeBase()
-	assert.Equal(t, cardA.Base().Level, 1)      // level from 0 to 1
-	assert.Equal(t, cardA.Base().Damage, 9)     // damage from 6 to 9
-	assert.Equal(t, Enemy, cardA.Base().Target) // if upgrade target not set, then not change it
-
-	assert.Equal(t, cardA.Current().Level, 1) // upgrade base will also update the current
-
-	cardA.UpgradeCurrent()
-	assert.Equal(t, cardA.Current().Level, 2) // upgrade the current
-	assert.Equal(t, cardA.Base().Level, 1)    // upgrade current will NOT update the base
-
-	// Copy the Card
-	cardACopy := cardA.Copy()
-	assert.Equal(t, 1, cardACopy.Base().Level)    // base status copied from cardA
-	assert.Equal(t, 1, cardACopy.Current().Level) // current status will NOT be copied from cardA, it's a copy of the base
-
-	cardB := CreateCardFunc["Defend"]()
-	assert.Equal(t, cardB.Name(), "Defend")
-	assert.Equal(t, cardB.Base().Damage, 0)
-	assert.Equal(t, cardB.Base().Block, 5)
-
-	cardB.UpgradeCurrent()
-	assert.Equal(t, cardB.Current().Level, 1)     // level from 0 to 1
-	assert.Equal(t, cardB.Current().Block, 8)     // damage from 6 to 9
-	assert.Equal(t, cardB.Current().Target, Self) // if upgrade target not set, then not change it
+type TestCard struct {
+	id   string
+	desc string
 }
 
-func TestPile(t *testing.T) {
-	random := rand.New(rand.NewSource(9012))
-	p, err := CreatePile([]string{"Strike", "Strike", "Strike", "Strike", "Strike"})
-
-	if err != nil {
-		t.Error(err)
-	}
-	assert.Equal(t, 5, p.Num())
-
-	// Draw
-	pp, err := CreatePile([]string{"Defend", "Defend", "Defend", "Defend", "Bash"})
-	pp.Draw(p, p.Num())
-	assert.Equal(t, 10, len(pp.cards))
-	assert.Equal(t, 0, len(p.cards))
-	assert.EqualError(t, pp.Draw(p, 1), "not enough card(s) to draw")
-	assert.EqualError(t, pp.Draw(p, 0), "draw number should be larger than 0")
-
-	// Pick
-	p.Pick(pp, pp.cards[0].ID())
-	assert.Equal(t, 1, p.Num())
-	assert.Equal(t, 9, pp.Num())
-	assert.Equal(t, "Defend", p.cards[0].Name())
-	p.Pick(pp, pp.cards[0].ID())
-	assert.Equal(t, 2, p.Num())
-	assert.Equal(t, 8, pp.Num())
-	assert.Equal(t, "Defend", p.cards[1].Name())
-
-	// Shuffle
-	pp.Shuffle(random)
-	assert.Equal(t, "Strike", pp.cards[0].Name())
-	assert.Equal(t, "Defend", pp.cards[7].Name())
+func (c *TestCard) Upgrade() error {
+	return errors.New("can't upgrade")
 }
 
-// 	pp, err := CreatePile(random, []string{"Defend", "Defend", "Defend", "Defend", "Bash"})
+func (c *TestCard) String() string {
+	return "<" + c.desc + ">"
+}
 
-// 	pp.Draw(len(pp.cards), p)
+func (c *TestCard) ID() string {
+	return c.id
+}
 
-// 	assert.Equal(t, 10, len(p.cards))
+func TestShuffle(t *testing.T) {
+	cards := Pile([]Card{
+		&TestCard{id: "a", desc: "card a"},
+		&TestCard{id: "b", desc: "card b"},
+		&TestCard{id: "c", desc: "card c"},
+		&TestCard{id: "d", desc: "card d"},
+		&TestCard{id: "e", desc: "card e"},
+		&TestCard{id: "f", desc: "card f"},
+	})
+	seed := rand.New(rand.NewSource(99))
+	cards.Shuffle(seed)
+	assert.Equal(t, "[<card f> <card a> <card b> <card c> <card e> <card d>]", fmt.Sprint(cards))
+}
 
-// 	assert.Equal(t, 0, len(pp.cards))
+func TestDraw(t *testing.T) {
+	a := Pile([]Card{
+		&TestCard{id: "a", desc: "card a"},
+		&TestCard{id: "b", desc: "card b"},
+		&TestCard{id: "c", desc: "card c"},
+	})
 
-// 	assert.EqualError(t, pp.Draw(1, p), "not enough card(s) to draw")
+	b := Pile([]Card{
+		&TestCard{id: "d", desc: "card d"},
+		&TestCard{id: "e", desc: "card e"},
+		&TestCard{id: "f", desc: "card f"},
+	})
 
-// 	assert.EqualError(t, pp.Draw(0, p), "draw number should be larger than 0")
+	a.Draw(&b)
+	assert.Equal(t, "[<card a> <card b> <card c> <card f>]", fmt.Sprint(a))
+	assert.Equal(t, "[<card d> <card e>]", fmt.Sprint(b))
 
-// init all the cards in the pile
+	a.Draw(&b)
+	a.Draw(&b)
+	assert.Equal(t, "[<card a> <card b> <card c> <card f> <card e> <card d>]", fmt.Sprint(a))
 
-// 	for _, card := range p.cards {
-// 		card.Init()
-// 	}
-// 	assert.Equal(t, 2, p.FindCardByID(p.cards[2].ID()))
-// 	assert.Equal(t, 10, p.CardsNum())
+	err := a.Draw(&b)
+	assert.Equal(t, ErrNotEnoughCards, err)
+}
 
-// 	card, err := p.RemoveCard(2)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+func TestPick(t *testing.T) {
+	a := Pile([]Card{
+		&TestCard{id: "a", desc: "card a"},
+		&TestCard{id: "b", desc: "card b"},
+		&TestCard{id: "c", desc: "card c"},
+		&TestCard{id: "d", desc: "card d"},
+		&TestCard{id: "e", desc: "card e"},
+		&TestCard{id: "f", desc: "card f"},
+	})
 
-// 	assert.Equal(t, 9, p.CardsNum())
-// 	assert.Equal(t, "[strike]", card.String())
+	b := Pile([]Card{
+		&TestCard{id: "g", desc: "card g"},
+		&TestCard{id: "h", desc: "card h"},
+	})
 
-// 	card, err = p.RemoveCard(2)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// 	assert.Equal(t, 8, p.CardsNum())
-// 	assert.Equal(t, "[defend]", card.String())
+	c := Pile([]Card{})
 
-// 	card, err = p.RemoveCard(7)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// 	assert.Equal(t, 7, p.CardsNum())
-// 	assert.Equal(t, "[bash]", card.String())
-// }
+	card, idx, _ := a.FindCard("a")
+	assert.Equal(t, "<card a>", card.String())
+	assert.Equal(t, 0, idx)
 
-// func TestStatusCopy(t *testing.T) {
+	b.Pick(&a, "d")
+	assert.Equal(t, "[<card g> <card h> <card d>]", fmt.Sprint(b))
+	assert.Equal(t, "[<card a> <card b> <card c> <card e> <card f>]", fmt.Sprint(a))
 
-// 	c := CreateCardFunc["Strike"]()
+	err := b.Pick(&a, "d")
+	assert.Equal(t, ErrCardNotExist, err)
 
-// 	s := c.Base().Copy()
-
-// 	assert.Equal(t, c.Base(), s)
-
-// 	t.Logf("origin -> %v, copied -> %v", c.Base(), s)
-
-// }
-
-// func TestCardCopy(t *testing.T) {
-// 	c := CreateCardFunc["Strike"]()
-// 	c.Init()
-// 	s := c.Copy()
-
-// 	// the base status pointer should be the same
-// 	assert.Equal(t, fmt.Sprintf("%p", c.Base()), fmt.Sprintf("%p", s.Base()))
-
-// 	// the current status pointer should not be the same
-// 	assert.NotEqual(t, fmt.Sprintf("%p", c.Current()), fmt.Sprintf("%p", s.Current()))
-
-// 	// like card [Ritual Dagger] -  if this card kills an enemy then permanently increase this card's damage by 3(5)
-// 	// if card[Ritual Dagger] upgraded in the battle, then original card in the deck will also be upgraded
-// 	// manager can use "base" status permanently change the card
-// 	s.Base().Damage = 100
-// 	assert.Equal(t, 100, c.Base().Damage)
-// }
-
-// // func TestManager(t *testing.T) {
-
-// // 	m, err := CreateManager([]string{"Strike", "Strike", "Strike", "Strike", "Strike", "Defend", "Defend", "Defend", "Defend", "Bash"})
-
-// // 	if err != nil {
-// // 		t.Error(err)
-// // 	}
-
-// // 	assert.Equal(t, 10, len(m.deck.cards))
-
-// // 	// if a card name not exist, then return an error, and clear the pile
-
-// // 	m, err = CreateManager([]string{"Strike", "Strike", "Strike", "Strike", "Strike", "Defend", "Defend", "ABC", "Defend", "Bash"})
-
-// // 	assert.EqualError(t, err, "create function for card [ABC] not found")
-
-// // 	// assert.Equal(t, 0, len(m.deck.cards))
-// // 	// create the deck
-
-// // 	m, err = CreateManager([]string{"Strike", "Strike", "Strike", "Strike", "Strike", "Defend", "Defend", "Defend", "Defend", "Bash"})
-
-// // 	if err != nil {
-// // 		t.Error(err)
-// // 	}
-
-// // 	// copy the deck cards into draw pile, and shuffle
-// // 	err = m.Shuffle()
-
-// // 	if err != nil {
-// // 		t.Error(err)
-// // 	}
-
-// // 	// t.Log(m.deck.cards, m.draw.cards)
-
-// // 	// [[bash] [defend] [defend] [strike] [strike] [strike] [strike] [defend] [defend] [strike]
-// // 	// draw 5 cards from draw pile to hand pile
-// // 	err = m.Draw(5)
-// // 	if err != nil {
-// // 		t.Error(err)
-// // 	}
-
-// // 	assert.Equal(t, 5, m.draw.CardsNum()) // [[bash] [defend] [defend] [strike] [strike]]
-// // 	assert.Equal(t, 5, m.hand.CardsNum()) // [[strike] [strike] [defend] [defend] [strike]]
-
-// // 	// exaust a card from hand pile
-// // 	eCard := m.hand.cards[2]
-// // 	err = m.Exaust(eCard)
-// // 	if err != nil {
-// // 		t.Error(err)
-// // 	}
-// // 	assert.Equal(t, 1, m.exaust.CardsNum()) // [[defend]]
-// // 	assert.Equal(t, 4, m.hand.CardsNum())   // [[strike] [strike] [defend] [strike]]
-// // 	assert.Equal(t, "[defend]", eCard.String())
-// // 	assert.Equal(t, m.exaust.cards[0], eCard)
-
-// // 	// discard a card from hand pile
-// // 	dCard := m.hand.cards[2]
-// // 	err = m.Discard(dCard)
-// // 	if err != nil {
-// // 		t.Error(err)
-// // 	}
-// // 	assert.Equal(t, 1, m.discard.CardsNum()) // [[defend]]
-// // 	assert.Equal(t, 3, m.hand.CardsNum())    // [[strike] [strike] [strike]]
-// // 	assert.Equal(t, "[defend]", dCard.String())
-// // 	assert.Equal(t, m.discard.cards[0], dCard)
-
-// // 	// create the deck
-// // 	m, err = CreateManager([]string{"Strike", "Strike", "Strike", "Strike", "Strike", "Defend", "Defend", "Defend", "Defend", "Bash"})
-// // 	if err != nil {
-// // 		t.Error(err)
-// // 	}
-
-// // 	// copy the deck cards into draw pile, and shuffle
-// // 	err = m.Shuffle()
-// // 	if err != nil {
-// // 		t.Error(err)
-// // 	}
-
-// // 	assert.Equal(t, 10, m.draw.CardsNum()) // [[bash] [defend] [defend] [strike] [strike] [strike] [strike] [defend] [defend] [strike]]
-
-// // 	err = m.Draw(5)
-// // 	if err != nil {
-// // 		t.Error(err)
-// // 	}
-
-// // 	assert.Equal(t, 5, m.hand.CardsNum()) // hand: [strike] [strike] [defend] [defend] [strike]
-// // 	err = m.ExaustAll()
-// // 	if err != nil {
-// // 		t.Error(err)
-// // 	}
-// // 	assert.Equal(t, 0, m.hand.CardsNum())
-// // 	assert.Equal(t, 5, m.exaust.CardsNum())
-
-// // 	err = m.Draw(5)
-// // 	assert.Equal(t, 5, m.hand.CardsNum()) // hand: [bash] [defend] [defend] [strike] [strike]
-// // 	err = m.DiscardAll()
-// // 	if err != nil {
-// // 		t.Error(err)
-// // 	}
-// // 	assert.Equal(t, 0, m.draw.CardsNum())
-// // 	assert.Equal(t, 0, m.hand.CardsNum())
-
-// // 	assert.Equal(t, 5, m.exaust.CardsNum())
-// // 	assert.Equal(t, 5, m.discard.CardsNum())
-
-// // 	if err = m.ReShuffle(); err != nil {
-// // 		t.Error(err)
-// // 	}
-
-// // 	assert.Equal(t, 0, m.discard.CardsNum())
-// // 	assert.Equal(t, 5, m.draw.CardsNum())
-// // }
+	err = b.Pick(&c, "d")
+	assert.Equal(t, ErrPileIsNilOrEmpty, err)
+}
